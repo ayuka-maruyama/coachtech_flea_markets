b2d87ab00e7b1c35820243ea05539fc5a8feccf8
ayuka <basketball110512@gmail.com>
2024-12-02 05:56:55 +0900

    共通部分の更新及び新規会員登録画面の作成
    app.blade.php 権限変更による更新
    register.blade.php 新規登録画面の新規作成
    common.css フォントサイズの変更、バリデーションエラーメッセージ用記述をcommon.cssへ移行
    register.css 新規会員登録画面の作成による新規作成

---
+++ src/resources/views/auth/register.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/register.css') }}">
+@endsection
+
+@section('content')
+<div class="register-area">
+    <h1 class="ttl">会員登録</h1>
+    <div class="register-form">
+        <form action="/register" method="post">
+            @csrf
+            <div class="form-group">
+                <label class="label" for="name">ユーザー名</label>
+                <input class="form-input" id="name" type="text" name="name">
+                <div class="error">
+                    @error('name')
+                    <p>{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="label" for="email">メールアドレス</label>
+                <input class="form-input" id="email" type="email" name="email">
+                <div class="error">
+                    @error('email')
+                    <p>{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="label" for="password">パスワード</label>
+                <input class="form-input" id="password" type="password" name="password">
+                <div class="error">
+                    @error('password')
+                    <p>{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <label class="label" for="password_confirmation">確認用パスワード</label>
+                <input class="form-input" id="password_confirmation" name="password_confirmation">
+                <div class="error">
+                    @error('password_confirmation')
+                    <p>{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            <div class="form-group">
+                <button type="submit" class="submit-btn">登録する</button>
+            </div>
+        </form>
+        <p class="login-link">
+            <a href="/login">ログインはこちら</a>
+        </p>
+    </div>
+</div>
+@endsection
