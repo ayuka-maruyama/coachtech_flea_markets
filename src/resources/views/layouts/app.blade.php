9e18543b1af29fe477d0ea2d15291a1241a8241a
ayuka <basketball110512@gmail.com>
2024-12-01 19:36:32 +0900

    ビューの共通部分（ヘッダー）の作成
    ビューで共通するヘッダーに関するbladeファイルおよびCSSファイルの作成(
    レスポンシブデザイン対応済）

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/destyle.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+    <title>COACHTECHフリマ</title>
+</head>
+
+<body>
+    <header class="header">
+        <div class="log">
+            <a href="{{ url('/') }}"> <!-- controller作成、web.php記述後、urlの部分をroute('home')に修正する -->
+                <img class="log-img" src="{{ asset('image/logo.svg') }}" alt="logo">
+            </a>
+        </div>
+
+        @if(!Request::is('register') && !Request::is('login'))
+        <div class="search-bar">
+            <form action="/search" method="get">
+                <input class="input-area" type="text" name="q" placeholder="なにをお探しですか？" value="{{ request('q') }}">
+            </form>
+        </div>
+
+        <nav class="nav">
+            <ul>
+                @if(Auth::check())
+                <form action="/logout" method="post">
+                    @csrf
+                    <li class="logout">
+                        <button class="nav-btn" type="submit">ログアウト</button>
+                    </li>
+                </form>
+                @else
+                <li class="login">
+                    <a href="/login" class="nav-btn">ログイン</a>
+                </li>
+                @endif
+                <li class="my-page">
+                    <a href="/mypage" class="nav-btn">マイページ</a>
+                </li>
+                <li class="exhibit">
+                    <a href="/sell" class="nav-btn">出品</a>
+                </li>
+            </ul>
+        </nav>
+        @endif
+    </header>
+</body>
+
+</html>
