51cabc083bd35c75704e04bd63be620a6757e276
ayuka <basketball110512@gmail.com>
2024-12-03 15:44:29 +0900

    再送信用ルートでエラーがでたため修正
    use文の追加（use Illuminate\Http\Request;）

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
+use Illuminate\Http\Request;
 
 Route::get('/', function () {
     return view('layouts/app');
