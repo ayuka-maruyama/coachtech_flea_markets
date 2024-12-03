6deca7f0b136e6550861dc7252ae282eea4c930d
ayuka <basketball110512@gmail.com>
2024-12-03 15:38:28 +0900

    web.phpの新規登録に関係する記述の作成
    新規登録画面の表示、メール認証による表示ルートの記述

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,29 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Foundation\Auth\EmailVerificationRequest;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/app');
 });
+
+Route::get('/register', function () {
+    return view('auth/register');
+});
+
+// メール確認通知の表示ルート
+Route::get('/email/verify', function () {
+    return view('auth.verify-email'); // カスタムビュー
+})->middleware(['auth'])->name('verification.notice');
+
+// メール確認処理ルート
+Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
+    $request->fulfill(); // メール認証を完了する
+    return redirect('/'); // 認証後のリダイレクト先
+})->middleware(['auth', 'signed'])->name('verification.verify');
+
+// 再送信ルート
+Route::post('/email/verification-notification', function (Request $request) {
+    $request->user()->sendEmailVerificationNotification();
+    return back()->with('message', 'Verification link sent!');
+})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
