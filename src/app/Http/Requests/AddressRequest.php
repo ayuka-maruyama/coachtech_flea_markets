158662c52046f07fcdddf917aa6a94ecd57c0132
ayuka <basketball110512@gmail.com>
2024-12-09 10:41:05 +0900

    住所、画像ファイルの場フォームバリデーションファイルの作成

--- src/app/Http/Requests/AddressRequest.php
+++ src/app/Http/Requests/AddressRequest.php
@@ -6,23 +6,29 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class AddressRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'profile_name' => 'required|string|max:255',
+            'postal_number' => 'required|regex:/^\d{7}$|^\d{3}-\d{4}$/',
+            'address' => 'required|string',
+            'building' => 'required|string',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'profile_name.required' => 'ユーザー名を入力してください',
+            'postal_number.required' => '郵便番号を入力してください',
+            'postal_number.regex' => '郵便番号は「123-4567」の形式で入力してください',
+            'address.required' => '住所を入力してください',
+            'building.required' => '建物名を入力してください',
         ];
     }
 }
--- src/app/Http/Requests/ProfileRequest.php
+++ src/app/Http/Requests/ProfileRequest.php
@@ -6,23 +6,24 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ProfileRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
     public function authorize(): bool
     {
         return true;
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
-            //
+            'profile_image' => 'file|image|mimes:jpeg,png',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'profile_image.file' => 'プロフィールで使用するファイルを選択してください',
+            'profile_image.image' => '画像ファイルを選択してください',
+            'profile_image.mimes' => '画像ファイルはjpegかpngのファイルを選択してください',
         ];
     }
 }
