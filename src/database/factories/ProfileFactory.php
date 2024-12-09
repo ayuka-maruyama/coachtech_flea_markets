f1441eacc116cca7f3c153a78f9e525b03f9fd4a
ayuka <basketball110512@gmail.com>
2024-12-09 10:03:45 +0900

    画像ファイルの設定が必須ではないため、記述内容を修正
    Factoryでprofile_imageでpublic/image/profile_**.pngを作成するようにしていたが、バリデーションの設定時に画像ファイルの指定が必須ではないことが分かったため、記述をnullに修正
    それに伴い、画像ファイルを削除

--- src/database/factories/ProfileFactory.php
+++ src/database/factories/ProfileFactory.php
@@ -7,27 +7,16 @@ use Carbon\Carbon;
 use App\Models\Profile;
 use App\Models\User;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Profile>
- */
 class ProfileFactory extends Factory
 {
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
-        // すでに関連するuser_idを除外してランダムな未使用のユーザーIDを取得
-        $unusedUserId = User::whereNotIn('id', Profile::pluck('user_id')->toArray()) // 既存のuser_idを除外
-            ->whereNotNull('email_verified_at') // メール認証済みユーザーのみ対象
+        $unusedUserId = User::whereNotIn('id', Profile::pluck('user_id')->toArray())
+            ->whereNotNull('email_verified_at')
             ->inRandomOrder()
             ->first();
 
-        // ユーザーが見つからない場合、エラー処理または新しいユーザーを作成する処理を加える
         if (!$unusedUserId) {
-            // 新しいユーザーを作成してそのIDを使用する
             $unusedUserId = User::factory()->create();
         }
 
@@ -36,8 +25,8 @@ class ProfileFactory extends Factory
             'postal_number' => preg_replace('/^(\d{3})(\d{4})$/', '$1-$2', fake()->postcode()),
             'address' => $this->faker->prefecture() . $this->faker->city() . $this->faker->streetAddress(),
             'building' => $this->faker->secondaryAddress(),
-            'profile_image' => 'public/image/profile_' . $this->faker->numberBetween(1, 2) . '.png', // 画像パスを設定
-            'user_id' => $unusedUserId->id, // ランダムに取得した未使用ユーザーIDを設定
+            'profile_image' => null,
+            'user_id' => $unusedUserId->id,
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
