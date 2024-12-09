31cb2111e2e3565a1f3afcc53182a11534a66df8
ayuka <basketball110512@gmail.com>
2024-12-10 08:32:14 +0900

    profile.jsに郵便番号の自動補正を追加

--- src/public/js/profile.js
+++ src/public/js/profile.js
@@ -8,8 +8,8 @@ document.getElementById("image").addEventListener("change", function (event) {
 
         reader.onload = function (e) {
             preview.src = e.target.result;
-            preview.style.display = "block";
-            previewArea.style.backgroundColor = "transparent"; // 背景色をリセット
+            preview.style.display = "block"; // 画像を表示
+            previewArea.style.backgroundColor = "transparent"; // 背景色を透明に変更
         };
 
         reader.readAsDataURL(file);
@@ -20,3 +20,18 @@ document.getElementById("image").addEventListener("change", function (event) {
         previewArea.style.backgroundColor = "gray"; // 背景色を灰色にする
     }
 });
+
+// 郵便番号の自動補正
+document
+    .getElementById("postal_number")
+    .addEventListener("input", function (e) {
+        let input = e.target.value;
+
+        // 全角数字を半角数字に変換
+        input = input.replace(/[０-９]/g, function (s) {
+            return String.fromCharCode(s.charCodeAt(0) - 0xfee0);
+        });
+
+        // 入力値を更新
+        e.target.value = input;
+    });
