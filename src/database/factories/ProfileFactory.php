57b6968c7781de40cc6a99297dcbf3afc71bab58
ayuka <basketball110512@gmail.com>
2024-12-17 09:56:23 +0900

    プロフィール用の画像URLなしからダミーデータ作成へ変更

--- src/database/factories/ProfileFactory.php
+++ src/database/factories/ProfileFactory.php
@@ -16,7 +16,7 @@ class ProfileFactory extends Factory
             'postal_number' => preg_replace('/^(\d{3})(\d{4})$/', '$1-$2', fake()->postcode()),
             'address' => $this->faker->prefecture() . $this->faker->city() . $this->faker->streetAddress(),
             'building' => $this->faker->secondaryAddress(),
-            'profile_image' => null,
+            'profile_image' => $this->faker->imageUrl(),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ];
