0d1bae5de2d2237c3e80b3b012e7e075f18ee8a2
ayuka <basketball110512@gmail.com>
2024-12-23 08:12:11 +0900

    機能要件の確認によるマイグレーションファイルの修正
    変更箇所
    ordersテーブルに購入時の送付先情報を追加

--- src/database/migrations/2024_11_26_194140_create_orders_table.php
+++ src/database/migrations/2024_11_26_194140_create_orders_table.php
@@ -6,9 +6,6 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('orders', function (Blueprint $table) {
@@ -16,13 +13,13 @@ return new class extends Migration
             $table->foreignId('item_id');
             $table->foreignId('user_id');
             $table->string('payment_method');
+            $table->string('postal_number');
+            $table->string('address');
+            $table->string('building');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('orders');
