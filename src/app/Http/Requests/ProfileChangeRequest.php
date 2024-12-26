13da848873d9e2c11261c3a7a3a946d9a26cea9f
ayuka <basketball110512@gmail.com>
2024-12-26 19:53:28 +0900

    送付先情報更新用のフォームバリデーションの作成

---
+++ src/app/Http/Requests/ProfileChangeRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class ProfileChangeRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'postal_number' => 'required|string|regex:/^(\d{3})-(\d{4})$/',
+            'address' => 'required|string|max:255',
+            'building' => 'required|string|max:255',
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'postal_number.required' => '郵便番号を入力してください',
+            'postal_number.regex' => '郵便番号は「123-4567」の形式で入力してください',
+            'address.required' => '住所を入力してください',
+            'building.required' => '建物名を入力してください',
+        ];
+    }
+}
