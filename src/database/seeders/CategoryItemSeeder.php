c697009a258afb5ed5a42e3ab342f73149172893
ayuka <basketball110512@gmail.com>
2024-12-31 07:07:58 +0900

    CategoryItemFactoryを廃止、CategoryItemSeederで中間テーブルに挿入するデータを作成
    Factoryを廃止した理由として、初期登録商品の商品とカテゴリーがランダムになってしまうこと
    初期登録商品も少ないことから、手動で紐づけを行うこととしたため

--- src/database/seeders/CategoryItemSeeder.php
+++ src/database/seeders/CategoryItemSeeder.php
@@ -2,27 +2,117 @@
 
 namespace Database\Seeders;
 
-use App\Models\CategoryItem;
-use App\Models\Item;
-use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
+
 
 class CategoryItemSeeder extends Seeder
 {
     public function run(): void
     {
-        Item::all()->each(
-            function ($item) {
-                // まず必ず1つのカテゴリーを選んで紐づけ
-                $firstCategory = Category::inRandomOrder()->first();
+        // Item::all()->each(
+        //     function ($item) {
+        //         // まず必ず1つのカテゴリーを選んで紐づけ
+        //         $firstCategory = Category::inRandomOrder()->first();
 
-                // 中間テーブルに追加
-                CategoryItem::create([
-                    'category_id' => $firstCategory->category_id,
-                    'item_id' => $item->item_id,
-                ]);
-            }
-        );
-        CategoryItem::factory()->count(5)->create();
+        //         // 中間テーブルに追加
+        //         category_item::create([
+        //             'category_id' => $firstCategory->category_id,
+        //             'item_id' => $item->item_id,
+        //         ]);
+        //     }
+        // );
+        // category_item::factory()->count(5)->create();
+        DB::table('category_item')->insert([
+            'category_id' => 1,
+            'item_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 5,
+            'item_id' => 1,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 2,
+            'item_id' => 2,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 10,
+            'item_id' => 3,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 1,
+            'item_id' => 4,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 5,
+            'item_id' => 4,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 2,
+            'item_id' => 5,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 2,
+            'item_id' => 6,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 4,
+            'item_id' => 7,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 4,
+            'item_id' => 8,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 5,
+            'item_id' => 8,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 10,
+            'item_id' => 8,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 10,
+            'item_id' => 9,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 4,
+            'item_id' => 10,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('category_item')->insert([
+            'category_id' => 6,
+            'item_id' => 10,
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
