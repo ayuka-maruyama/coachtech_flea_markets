af66df513f81109d68ae6ae88842c3c7d0eebe16
ayuka <basketball110512@gmail.com>
2024-12-31 07:05:45 +0900

    CategorySeederに初期設定データを追加

--- src/database/seeders/CategorySeeder.php
+++ src/database/seeders/CategorySeeder.php
@@ -11,17 +11,17 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            'category_name' => '食品',
+            'category_name' => 'ファッション',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
         DB::table('categories')->insert([
-            'category_name' => '洋服',
+            'category_name' => '家電',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
         DB::table('categories')->insert([
-            'category_name' => 'メンズ',
+            'category_name' => 'インテリア',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
@@ -31,22 +31,52 @@ class CategorySeeder extends Seeder
             'updated_at' => Carbon::now(),
         ]);
         DB::table('categories')->insert([
-            'category_name' => '靴',
+            'category_name' => 'メンズ',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
         DB::table('categories')->insert([
-            'category_name' => '食器',
+            'category_name' => 'コスメ',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
         DB::table('categories')->insert([
-            'category_name' => 'メイク',
+            'category_name' => '本',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
         DB::table('categories')->insert([
-            'category_name' => 'カバン',
+            'category_name' => 'ゲーム',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('categories')->insert([
+            'category_name' => 'スポーツ',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('categories')->insert([
+            'category_name' => 'キッチン',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('categories')->insert([
+            'category_name' => 'ハンドメイド',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('categories')->insert([
+            'category_name' => 'アクセサリー',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('categories')->insert([
+            'category_name' => 'おもちゃ',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+        DB::table('categories')->insert([
+            'category_name' => 'ベビー・キッズ',
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
         ]);
