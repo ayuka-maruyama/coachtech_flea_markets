4a1647aca52626689a923a1ff71b1502035057a1
ayuka <basketball110512@gmail.com>
2024-12-30 13:32:53 +0900

    リレーション用のIDカラム名を設定

--- src/app/Models/Order.php
+++ src/app/Models/Order.php
@@ -24,7 +24,7 @@ class Order extends Model
 
     public function item()
     {
-        return $this->belongsTo(Item::class);
+        return $this->belongsTo(Item::class, 'item_id');
     }
 
     public function payment()
