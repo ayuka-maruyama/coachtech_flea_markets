261ebbb83e805a1c40a1e404c723d1782adeceb3
ayuka <basketball110512@gmail.com>
2025-01-11 08:49:56 +0900

    使用していない機能の削除

--- src/app/Providers/FortifyServiceProvider.php
+++ src/app/Providers/FortifyServiceProvider.php
@@ -3,16 +3,9 @@
 namespace App\Providers;
 
 use App\Actions\Fortify\CreateNewUser;
-use App\Actions\Fortify\ResetUserPassword;
-use App\Actions\Fortify\UpdateUserPassword;
-use App\Actions\Fortify\UpdateUserProfileInformation;
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
+
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Str;
 use Laravel\Fortify\Fortify;
-use App\Http\Models\User;
 
 class FortifyServiceProvider extends ServiceProvider
 {
@@ -35,34 +28,9 @@ class FortifyServiceProvider extends ServiceProvider
         });
         config(['fortify.home' => '/email/verify']);
 
-        // Fortify::authenticateUsing(function ($request) {
-        //     // 認証されているユーザーを取得
-        //     $user = User::where('email', $request->email)->first();
-
-        //     // メール認証が未完了の場合、リダイレクト
-        //     if ($user && !$user->hasVerifiedEmail()) {
-        //         return null; // メール認証が未完了の場合、認証を無効にする
-        //     }
-
-        //     return $user; // メール認証が完了していれば、通常通り認証する
-        // });
-
         Fortify::loginView(function () {
-            return view('auth.login'); // ログイン画面
+            return view('auth.login');
         });
 
-        // Fortify::updateUserProfileInformationUsing(UpdateUserProfileInformation::class);
-        // Fortify::updateUserPasswordsUsing(UpdateUserPassword::class);
-        // Fortify::resetUserPasswordsUsing(ResetUserPassword::class);
-
-        // RateLimiter::for('login', function (Request $request) {
-        //     $throttleKey = Str::transliterate(Str::lower($request->input(Fortify::username())) . '|' . $request->ip());
-
-        //     return Limit::perMinute(5)->by($throttleKey);
-        // });
-
-        // RateLimiter::for('two-factor', function (Request $request) {
-        //     return Limit::perMinute(5)->by($request->session()->get('login.id'));
-        // });
     }
 }
