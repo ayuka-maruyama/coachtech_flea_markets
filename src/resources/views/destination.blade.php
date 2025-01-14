c7f17564ce4f9938d898bfb3688428e44f30ecd8
ayuka <basketball110512@gmail.com>
2025-01-14 11:35:37 +0900

    読み込み不要なCSSを削除

--- src/resources/views/destination.blade.php
+++ src/resources/views/destination.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/auth/register.css') }}">
-<link rel="stylesheet" href="{{ asset('css/auth/profile.css') }}">
 <link rel="stylesheet" href="{{ asset('css/destination.css') }}">
 @endsection
 
